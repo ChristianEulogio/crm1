603fb21fb8c365d31ecf30d6e53fd0b75d243656
Christian Eulogio <chrtaw@outlook.com>
2021-10-21 00:09:21 -0500

    One To Many & Many to Many

--- accounts/models.py
+++ accounts/models.py
@@ -11,6 +11,12 @@ class Customer(models.Model):
     #this is for return the name in the admin data bases
     def __str__(self):
         return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    #this is for return the name in the admin data bases
+    def __str__(self):
+        return self.name
         
 class Product(models.Model):
     CATEGORY = (
@@ -20,9 +26,13 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices= CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now=True, null=True)
+    tags = models.ManyToManyField(Tag)
 
+    #this is for return the name in the admin data bases
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     STATUS = (
@@ -30,7 +40,8 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),
     )
-    #customer =
-    #product =
+    customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
     date_created = models.DateTimeField(auto_now=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+    
