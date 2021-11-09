b8d98c20d74459725366880f3c39d2863da6515d
Christian Eulogio <chrtaw@outlook.com>
2021-11-09 12:37:59 -0600

    User Registration and Login Authentication

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,19 @@
 from django.forms import ModelForm
+#importamos la plantilla para crear el user
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
 from .models import Order
+
 
 class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields= '__all__'
+
+#del formulario usercreationform lo modificamos y agregamos email 2 
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,68 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
-from .models import *
-from .forms import OrderForm
-from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm
+#para el inico de secion y cierre
+from django.contrib.auth import authenticate, login, logout
+# messages flash
+from django.contrib import messages 
+#para requerir el login
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+from .models import *
+from .forms import OrderForm, CreateUserForm
+from .filters import OrderFilter
+
+def registerPage(request):
+    #si el usuario esta autentiado-logeado manda a la pagina home 
+    if request.user.is_authenticated:
+        return redirect('home')
+    #si no lo esta significa que apenas se esta registrando
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            #al darle submit le pasamos la informacion 
+            #del form en request.POST 
+            form = CreateUserForm(request.POST)
+            #si el form es valido save
+            if form.is_valid():
+                form.save()
+                #name of user
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+                #redirect login/
+                return redirect('login')
+
+        context = {'form':form}
+        return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        #si le damos en login se obtendra con request.POST.get()
+        #el user y la pass que metimos  
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+        #verificara con authenticate si coinciden con los datos de la base
+            user = authenticate(request, username=username, password=password)
+        #si los datos coinciden user no sera None
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        context = {}
+        return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+#si no estas logiado te manda a login
+@login_required(login_url='login')
 
 def home(request):
     orders= Order.objects.all()
@@ -20,12 +78,14 @@ def home(request):
     'total_orders':total_orders,'delivered':delivered,
     'pending':pending}
     return render(request, 'accounts/dashboard.html', context)
+@login_required(login_url='login')
 
 def products(request):
     #use the queryDemos gracias a que tenemos los modelos importados
     products = Product.objects.all()
     #pass these into the template to the render function
     return render(request, 'accounts/products.html',{'products':products})
+@login_required(login_url='login')
 
 def customer(request, pk_test):
     customer = Customer.objects.get(id=pk_test)
@@ -39,6 +99,7 @@ def customer(request, pk_test):
     context = {'customer':customer, 'orders':orders, 
     'order_count':order_count, 'myFilter':myFilter}
     return render(request, 'accounts/customer.html', context)
+@login_required(login_url='login')
 
 def createOrder(request, pk):
 
@@ -57,6 +118,7 @@ def createOrder(request, pk):
 
     context = {'formset':formset}
     return render(request, 'accounts/order_form.html', context)
+@login_required(login_url='login')
 
 def updateOrder(request, pk):
     #para rellenar los  campos en la actualizacion
@@ -71,6 +133,7 @@ def updateOrder(request, pk):
 
     context = {'form':form}
     return render(request, 'accounts/order_form.html', context)
+@login_required(login_url='login')
 
 def deleteOrder(request, pk):
     order= Order.objects.get(id = pk)
